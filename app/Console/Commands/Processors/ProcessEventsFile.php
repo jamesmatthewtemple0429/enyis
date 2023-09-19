47803ba87008ab3b6f9c774d0f41c75b56241801
James M. Temple <development@jamesmatthewtemple.com>
2023-09-18 23:32:08 -0400

    Fix

--- app/Console/Commands/Processors/ProcessEventsFile.php
+++ app/Console/Commands/Processors/ProcessEventsFile.php
@@ -36,11 +36,11 @@ class ProcessEventsFile extends Command
             \Excel::import(new EventImport($ingest), 'events.csv','lists');
 
             RccEvent::where('entered_at','<',now()->subDays(30)->setTime(0,0,0))
-                ->where('subject','Event')
+                ->where('name','Event')
                 ->update(['ingest_id' => $ingest->id]);
 
             Ingest::where('name','Event')->where('id','!=', $ingest->id)->delete();
-            
+
             File::delete(storage_path('lists/events.csv'));
         }
 
